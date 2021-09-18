9af6597a1f2f741515d0299b9e59b4911aee5376
Dylan <dylanmckibbin@gmail.com>
2021-09-19 00:51:27 +1000

    update

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ENTER KEY HERE!"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "ENTER KEY HERE!"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "73c2e84635f53fa71667aaaeb6e5a97a"
+weather_api_key = "ENTER KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyD2qCFu_d52Eb3guOI1IUay1_mfbf3bDzY"
+g_key = "ENTER KEY HERE"
